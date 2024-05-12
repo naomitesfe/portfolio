afc8c1d027f5669205c4a2ac6a5ecc6af4856114
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2024-05-12 14:46:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
 import Education from "./Components/Education";
+import Experience from "./Components/Eperience";
 
 import "./styles.css";
 
