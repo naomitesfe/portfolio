1ea0f56222132fbb4f62523ec15e9b73fa14ec9c
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2024-05-12 06:29:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nuhamin Tesfaye",
+  title: "Computer Engineer and Cloud Consultant",
+  email: "tesfayenuhamin@gmail.com",
+  gitHub: "https://www.github.com/naomitesfe/",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/nuhamintesfaye",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
