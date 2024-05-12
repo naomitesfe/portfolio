7bcd3cf57d543d8bdd98d1add8e0136fe9853d8b
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2024-05-12 14:45:52 +0300

    Create Experience.jsx

---
+++ src/Components/Experience.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Experience = () => {
+    return(
+        <section className="experience" id="experience">
+            <h2>Experience</h2>
+            <div className="experience-section">
+                <h3>Software Developer</h3>
+                <p>Charis Technologies</p>
+                <p>Apr 2024 - Present</p>
+                <p>- Design, develop and deploy an advanced telegram bot backed with SQL database and AI recommendation system integrated with web and various paymemt api's.
+<br>- Design, develop and deploy various advanced software solutions.</br></p>
+            </div>
+            <div className="experience-section">
+                <h3>Cloud Consultant</h3>
+                <p>Nedamco Africa</p>
+                <p>Oct 2022 - May 2024</p>
+                <p>- Designed, implemented, and managed cloud solutions using AWS and Microsoft Azure platforms.
+<br>- Collaborated with other engineers and consultants to deliver high-quality cloud services and solutions.</br></p>
+            </div>
+            <div className="experience-section">
+                <h3>IT Technician</h3>
+                <p>Company Name</p>
+                <p>Location</p>
+                <p>Jan 2022 - Jul 2022</p>
+                <p>- Installed, configured, and maintained hardware and software systems for employees over 10 departments. 
+<br>- Troubleshot and resolved technical issues related to network connectivity, security, performance, and data backup.</br>
+<br>- Documented and updated IT policies, procedures, and manuals, adhering to best practices and standards.</br></p>
+            </div>
+        </section>
+    )
+}
+
+export default Experience;
