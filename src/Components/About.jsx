ad58fedc01ca750064315d1e96a407b94098cd0d
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2024-05-12 06:32:05 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Engineer and Cloud Consultant. I excel in devising inventive solutions and implementing user-friendly interfaces that combine simplicity with creativity.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -34,19 +34,17 @@ const skillsList = [
   "Web design",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Azure",
+  "AWS",
+  "Leadership",
 ];
-
 /**
  * Use this to give more information about what you are passionate about,
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my expertise in cloud computing and computer engineering, I continually seek new and better ways to make technology accessible to all.";
 const About = () => {
   return (
     <section className="padding" id="about">
