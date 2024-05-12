d86ad08437115fb09c43171dc1acf3942296346c
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2024-05-12 14:28:26 +0300

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -9,3 +9,26 @@ const Education = () => {
 }
 
 export default Education;
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <div className="education-section">
+                <h3>Aspire Leaders/Organizational Leadership</h3>
+                <p>An immersive leadership journey, professional skills development training, an exclusive course designed and delivered by Harvard faculty, and interactive seminars led by world-class educators.</p>
+            </div>
+            <div className="education-section">
+                <h3>Computer Engineer</h3>
+                <p>Bsc in Electrical and Computer Engineering</p>
+            </div>
+            <div className="education-section">
+                <h3>GED in Natural Science</h3>
+                <p>Masha High School</p>
+            </div>
+        </section>
+    )
+}
+
+export default Education;
